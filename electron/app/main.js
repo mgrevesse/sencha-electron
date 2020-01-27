8749af8b4b5ffdb4dc979e46897032acf8431b8e
Michel Grevesse <michel@grevesse.com>
2020-01-27 15:55:57 +0100

    Update main.js

--- electron/app/main.js
+++ electron/app/main.js
@@ -8,7 +8,10 @@ function createWindow() {
     mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
     // and load the index.html of the app.
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
+    //mainWindow.loadURL(`file://${__dirname}/index.html`)
+    
+    //source dev folder
+    mainWindow.loadURL(`file://${__dirname}/../../index.html`)
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
